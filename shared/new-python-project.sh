d0fae8d3e916a7b571d1ba583740b958ff4f0298
kersanszki-levente <kersanszki.levente@gmail.com>
2024-12-24 13:16:07 +0100

    Added new uv based python project initialization

---
+++ shared/new-python-project.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if ! [ -x "$(command -v uv)" ]; then
+	echo "uv is not installed"
+	exit 1
+fi
+
+if ! [ -x "$(command -v fzf)" ]; then
+	echo "fzf is not installed"
+	exit 1
+fi
+
+select_python_version() {
+	uv python list | \
+        mawk '{ split($0, arr); print arr[1] }' | \
+        fzf
+}
+
+setup_pyright() {
+echo "
+[tool.pyright]
+exclude=[ \".venv\" ]
+venvPath=\".\"
+venv=\".venv\"" >> pyproject.toml
+}
+
+read -p "Name: " venv_name; \
+    selected_version=$(select_python_version); \
+    uv init --no-workspace --name "$venv_name" "$venv_name" --python "$selected_version"; \
+    cd "$(pwd)/$venv_name"; \
+    uv venv; \
+    setup_pyright
